650495323e621f73fefe57039e8640bb14c366be
yzq <1149635502@qq.com>
2019-03-18 15:40:16 +0800

    修复RSA中秘钥长度为2048时解密失败的问题，把MAX_DECRYPT_BLOCK修改为256即可

--- src/utils/RSAUtils.java
+++ src/utils/RSAUtils.java
@@ -11,65 +11,43 @@ import java.security.spec.X509EncodedKeySpec;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- */
-
-/**
- * <p>
- * RSA公钥/私钥/签名工具包
- * </p>
- * <p>
- * 罗纳德·李维斯特（Ron [R]ivest）、阿迪·萨莫尔（Adi [S]hamir）和伦纳德·阿德曼（Leonard [A]dleman）
- * </p>
- * <p>
- * 字符串格式的密钥在未在特殊说明情况下都为BASE64编码格式<br/>
- * 由于非对称加密速度极其缓慢，一般文件不使用它来加密而是使用对称加密，<br/>
- * 非对称加密算法可以用来对对称加密的密钥加密，这样保证密钥的安全也就保证了数据的安全
- * </p>
- *
- * @author IceWee
- * @date 2012-4-26
- * @version 1.0
- */
 public class RSAUtils {
 
-    /** */
+
     /**
      * 加密算法RSA
      */
-    public static final String KEY_ALGORITHM = "RSA";
+    private static final String KEY_ALGORITHM = "RSA";
 
-    /** */
     /**
      * 签名算法
      */
     // public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
-    public static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+    private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
 
-    /** */
     /**
      * 获取公钥的key
      */
-    public static final String PUBLIC_KEY = "RSAPublicKey";
+    private static final String PUBLIC_KEY = "RSAPublicKey";
 
-    /** */
+
     /**
      * 获取私钥的key
      */
-    public static final String PRIVATE_KEY = "RSAPrivateKey";
+    private static final String PRIVATE_KEY = "RSAPrivateKey";
 
-    /** */
+
     /**
      * RSA最大加密明文大小
      */
     private static final int MAX_ENCRYPT_BLOCK = 117;
 
-    /** */
+
     /**
      * RSA最大解密密文大小
      */
-    private static final int MAX_DECRYPT_BLOCK = 128;
+   // private static final int MAX_DECRYPT_BLOCK = 128;
+    private static final int MAX_DECRYPT_BLOCK = 256;
 
 
     public static void main(String[] args) {
@@ -78,6 +56,13 @@ public class RSAUtils {
         /*RSA  1024 */
         String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIarYvrIMZGHKa8f2E6ubg0//28R1zJ4ArD+XELXYvDrM8UBR42PqJCpjPN3hC91YAnnk2Y9U+X5o/rGxH5ZTZzYy+rkAmZFJa1fK2mWDxPYJoxH+DGHQc+h8t83BMB4pKqVPhcJVF6Ie+qpD5RFUU/e5iEz8ZZFDroVE3ubKaKwIDAQAB";
         String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIhqti+sgxkYcprx/YTq5uDT//bxHXMngCsP5cQtdi8OszxQFHjY+okKmM83eEL3VgCeeTZj1T5fmj+sbEfllNnNjL6uQCZkUlrV8raZYPE9gmjEf4MYdBz6Hy3zcEwHikqpU+FwlUXoh76qkPlEVRT97mITPxlkUOuhUTe5sporAgMBAAECgYA0aSND37iifKUTaKOpXIKFoI23910EMAnrAXmaTIkafUBZjL7Ay0Q+QIcDHeGjgNlW9YvGXMbB5wMhMYKMgOUV1FpeqQdDslO4Z7zynRjkDJkjOKkE2/j10CvmNO8e2uCWKsYYUE9IyTkxcypjBCv16ifT0qmdxb7uKLccYI16eQJBANMutfNO/q7kUKiYvilBLN9+pZOg6eTmKmV0Xygoa3ClpQTfurwLA8W/Fv3oXnjHXTryNVHeoxSH69imo0RZ9kcCQQClXhMbXlfvl5iInmwziFhtYBztvkLuyQ084FgszR7iR0nuOWoURLQa5O7sLL724FNRlSvOCmmmWguh2vmQgRr9AkBDS5tHkWCvMqpRT3spgk9eWOlChgCCpKXV9qNsFJVILEDNsM28pnXpSd91wdp4+m7HHe/Hyv6EyFtrio50dYZ5AkAODVVwUO8GBArJKTUml+JzwOQUa8OCSQFf9+xmOjPypH4qySQzfrcTRfrrhM3haqSJ3TQwuP/LTAGLCnGEjwP9AkBqFFyrrQviPOhwel3NWjRv8mftOFgnm0Isk/NQJ4JtoahYvPDeUyP80WSuVWnPyV4zHz9Kw7BggYCPc4xZDACV";
+
+
+        /*RSA 2048*/
+
+//        String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAichGTEP0QFswnvn+ZAQrgGHM8VeDZLJuezGhgxh4d9SyRUfnIW/zefT71rwS4bZUs1MPxJwavOyxABJOHLuckdHXknCsGEWz78gsA6D0+O+9dl1gCZR29nnN/NlzmNbSjFnzvsTJYBlS88qSr35RXFE+6DM7uPsS8Fm2I+65FteJ8p2yMvpSg72QkIX8xvI1F1uwXrciIB+4u7uTozxIplMOo4a6uhAm3W+Kjpz3ni2btjGqHRbqb3ebSZyl+nFfnjQaBe3XyVxAWDSanjgFj/wbqbeug9FBs+nQFVPIZR9z0aE5Ndi5o3eSkV7HFmWpkxaiPZ0BLRK3XHMaBtuSpwIDAQAB";
+//        String privateKey = "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCJyEZMQ/RAWzCe+f5kBCuAYczxV4Nksm57MaGDGHh31LJFR+chb/N59PvWvBLhtlSzUw/EnBq87LEAEk4cu5yR0deScKwYRbPvyCwDoPT47712XWAJlHb2ec382XOY1tKMWfO+xMlgGVLzypKvflFcUT7oMzu4+xLwWbYj7rkW14nynbIy+lKDvZCQhfzG8jUXW7BetyIgH7i7u5OjPEimUw6jhrq6ECbdb4qOnPeeLZu2MaodFupvd5tJnKX6cV+eNBoF7dfJXEBYNJqeOAWP/Bupt66D0UGz6dAVU8hlH3PRoTk12Lmjd5KRXscWZamTFqI9nQEtErdccxoG25KnAgMBAAECggEBAIPz1b88ZTMtIgdejA7lH3Q4Nbn8gc1yRPSet3uBd/3rKT/IeMZBHQBzaqxgOgUIRV3n8nXsun6sf2b+IOjLlErimH2agnZMauL85YokH/g4QU6WZl9GXBf41xmMd3SsZ8AadaEBfYoXNqZcHtcLNogfFwvx5QRnD+A3SoRnH8OLBeVvOEe4AqHLT2xEZ9TeCf3fJe0Rf0fUIbw7I5ioiRZV/ir0L1VM7+1k2JODUkdC2Luj5Tl3nl1Eg6EmkYCmGE1bip1NAatsfjPBLMF7XdPNjLboiffjgKVBOjb7Y9vL18BCoLtWeTT2GkMpi5Sr94T1te1Ox77dF4BP33Xn7eECgYEA1TNUrAQsh14NbbkwFtUHXS8/YXt81p9wbSpFBymIawF2Lkk0913TB4CHSun45LhYXjdZZxK/TgqC5EIq5v2RA0jY3cSxoqVe6RZKB04E8wszeJHiEJPdu2vFnpZh9iAyhswiM5FmuKZKoWsVc2SZrBXAI02smSn3lXYok1VBS3sCgYEApXEZS6gjUu4o7ZL53Ur1HDfi/nxpkxqrPh+D1HVYjzjT+4vTeZwtLXt2VCInPWNXH+f11mzhxIrLkI0jMcSCah81DuU8aFXnqvPuyFvt9uaQBYlVWBtkcGZyeaxHFrbfCyeu0jm7SfwmiIg12hKlIHtPTjEZQUX+kkWr8cdaZ8UCgYEAh0Pl+K09QzVc97yC0jmeTnTnlYWvksvdnKUw3nZvYtSukndH75nLhfr524HOs+5xwnUDd+3hCjaJDSEd7yf5lUfmr+1XdoXNTb0igrfxU/JLWbfU4geuqnaaDyACTxHmfLePC4C413ZJ61fxaCDvjsrN+JgTZanGt0EcRT3WC3kCgYEAgf5/GMJxlw0JXbs515a5R8Xl9358Whj/at3KcRsPTeIiNqnkrc54dR9ol60KViMDZ0+VDDobn5pLXzZ26/jzXD1PLHgU4gp18Q6glhAdx/3cNm11gLhtUCA/XLlwVjm0wggZRpgUQIr/IBKe9c3mr8IUS2Uq6e38nKRf+adhst0CgYAM4tvl+U1MPbbz3YzDv8QPepZ7Pglgdfxqfr5OkXA7jNhqTZjSq10B6oClGvirBo1m6f26F02iUKk1n67AuiLlTP/RRZHi1cfq6P9IaXl23PcxJfUMvIxQDS0U+UTFpNXryTw/qNAkSfufN48YzKdGvc8vHrYJyaeemaVlbdJOCw==";
+
 
         try {
             //  Map<String, Object> keyMap = RSAUtils.genKeyPair();
@@ -97,15 +82,15 @@ public class RSAUtils {
             byte[] privateKeyEncryptBytes = RSAUtils.encryptByPrivateKey(data.getBytes(), privateKey);
             System.out.println("私钥加密后的数据：" + Base64.encode(privateKeyEncryptBytes));
 
-//            String singnData = RSAUtils.sign(data.getBytes(), privateKey);
-//            System.out.println("私钥签名后的数据：" + singnData);
+            String singnData = RSAUtils.sign(data.getBytes(), privateKey);
+            System.out.println("私钥签名后的数据：" + singnData);
 
 
             byte[] publicDecryptBytes = RSAUtils.decryptByPublicKey(privateKeyEncryptBytes, publicKey);
             System.out.println("公钥解密后的数据：" + new String(publicDecryptBytes));
 
-//            boolean isSign = RSAUtils.verify(data.getBytes(), publicKey, singnData);
-//            System.out.println("签名是否正确：" + isSign);
+            boolean isSign = RSAUtils.verify(data.getBytes(), publicKey, singnData);
+            System.out.println("签名是否正确：" + isSign);
 
 
         } catch (Exception e) {
@@ -140,7 +125,6 @@ public class RSAUtils {
         return keyMap;
     }
 
-    /** */
     /**
      * <p>
      * 用私钥对信息生成数字签名
@@ -165,7 +149,7 @@ public class RSAUtils {
         return Base64.encode(signature.sign());
     }
 
-    /** */
+
     /**
      * <p>
      * 校验数字签名
@@ -193,7 +177,7 @@ public class RSAUtils {
         return signature.verify(Base64.decode(sign));
     }
 
-    /** */
+
     /**
      * <P>
      * 私钥解密
@@ -238,7 +222,7 @@ public class RSAUtils {
         return decryptedData;
     }
 
-    /** */
+
     /**
      * <p>
      * 公钥解密
@@ -279,7 +263,6 @@ public class RSAUtils {
         return decryptedData;
     }
 
-    /** */
     /**
      * <p>
      * 公钥加密
@@ -321,7 +304,7 @@ public class RSAUtils {
         return encryptedData;
     }
 
-    /** */
+
     /**
      * <p>
      * 私钥加密
@@ -362,7 +345,7 @@ public class RSAUtils {
         return encryptedData;
     }
 
-    /** */
+
     /**
      * <p>
      * 获取私钥
@@ -378,7 +361,7 @@ public class RSAUtils {
         return Base64.encode(key.getEncoded());
     }
 
-    /** */
+
     /**
      * <p>
      * 获取公钥
@@ -394,32 +377,7 @@ public class RSAUtils {
         return Base64.encode(key.getEncoded());
     }
 
-    /**
-     * java端公钥加密
-     */
-    public static String encryptedDataOnJava(String data, String PUBLICKEY) {
-        try {
-            data = Base64.encode(encryptByPublicKey(data.getBytes(), PUBLICKEY));
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return data;
-    }
 
-    /**
-     * java端私钥解密
-     */
-    public static String decryptDataOnJava(String data, String PRIVATEKEY) {
-        String temp = "";
-        try {
-            byte[] rs = Base64.decode(data);
-            temp = new String(RSAUtils.decryptByPrivateKey(rs, PRIVATEKEY), "UTF-8");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return temp;
-    }
 
 }
